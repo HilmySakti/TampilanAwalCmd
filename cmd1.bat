9aef7acc7ccc09fb3be908e68d7a573168df2c1d
ヒルミーサクティ <74033002+HilmySakti@users.noreply.github.com>
2021-01-01 20:51:31 +0700

    Taruh file ke direktori D:\

---
+++ cmd1.bat
@@ -0,0 +1,21 @@
+@echo off
+Title MySistem
+cls
+color 0a
+echo.
+echo ################################################## #########################
+echo ## Biarlah kami mendapatkan ilmu layaknya kalian, karena ilmu merupakan ##
+echo ## salah satu kebebasan bagi kami, ##
+echo ## jangan biarkan masih ada orang yang menangis hanya karena tidak bisa ##
+echo ## membayar untuk mendapatkan ilmu ##
+echo ################################################## #########################
+echo ## ABOUT ME: ##
+echo ## ##
+echo ## * Name : HilmySakti ##
+echo ## * Github : https://github.com/HilmySakti ##
+echo ## * YouTube : HILMY GAMNG 87 ##
+echo ## ##
+echo ################################################## #########################
+echo ## Knowledge is just like a two-edged sword, use it wisely ##
+echo ################################################## #########################
+
